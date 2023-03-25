bf59d37e86ef597712b35c8524005998ee9450c2
Chan Ui <hcu55@naver.com>
2023-03-25 21:06:44 +0900

    Study initState and dispose method

--- lib/screens/screenb.dart
+++ lib/screens/screenb.dart
@@ -1,19 +1,45 @@
 import 'package:flutter/material.dart';
 
-class ScreenB extends StatelessWidget {
+class ScreenB extends StatefulWidget {
   const ScreenB({Key? key}) : super(key: key);
 
   @override
+  State<ScreenB> createState() => _ScreenBState();
+}
+
+class _ScreenBState extends State<ScreenB> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print('initState is called');
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    print('dispose is called');
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print('build is called');
     return Scaffold(
       appBar: AppBar(
           title: const Text("ScreenB")
       ),
-      body: const Center(
-        child: Text("ScreenB",
-          style: TextStyle(
-              fontSize: 24
+      body: Center(
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              primary: Colors.redAccent,
+            onPrimary: Colors.black
           ),
+          child: const Text('Go to Screen A'),
+          onPressed: () {
+            Navigator.pop(context);
+          },
         ),
       ),
     );
